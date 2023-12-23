1268d36b5ed00b334d2aeae7ec8caa2ef1cbe304
hojang <109591135+26solitude@users.noreply.github.com>
2023-12-23 20:45:52 +0900

    [Bronze II] Title: 방 배정, Time: 136 ms, Memory: 14628 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -7,26 +7,24 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int stdNum = Integer.parseInt(st.nextToken());
         int roomNum = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[2][7];
 
-        for(int i=0; i<stdNum; i++){
-            String stdinput = br.readLine();
-            String[] stdinputs = stdinput.split(" ");
-            arr[Integer.parseInt(stdinputs[0])][Integer.parseInt(stdinputs[1])]++;
+        for (int i = 0; i < stdNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]++;
         }
 
         int totalRoom = 0;
-        for(int i=0; i<2; i++){
-            for(int j=0; j<7; j++){
-                if(arr[i][j] != 0){
-                    if(arr[i][j]>roomNum) totalRoom += arr[i][j]/roomNum+1;
-                    else totalRoom++;
-                }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (arr[i][j] == 0) continue;
+
+                if (arr[i][j] % roomNum != 0) totalRoom++;
+                totalRoom += arr[i][j] / roomNum;
             }
         }
 

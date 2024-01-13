082bf93a98a0ee0ba48d38090ddef2354d4ffed2
hojang <109591135+26solitude@users.noreply.github.com>
2024-01-13 18:52:31 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,36 +1,3 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//    static int[] arr;
-//    static int N, M;
-//
-//    public static void main(String[] args) {
-//        Scanner s = new Scanner(System.in);
-//
-//        N = s.nextInt();
-//        M = s.nextInt();
-//
-//        arr = new int[M];
-//
-//        dfs(0);
-//    }
-//
-//    private static void dfs(int depth) {
-//        if(depth == M){
-//            for(int i=0; i<M; i++){
-//                System.out.print(arr[i] + " ");
-//            }
-//            System.out.println();
-//            return;
-//        }
-//
-//        for(int i=1; i<=N; i++){
-//            arr[depth] = i;
-//            dfs(depth+1);
-//        }
-//    }
-//}
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
